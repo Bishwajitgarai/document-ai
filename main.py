ae30281fe359d39c19843ca4f4d876f033bc94a3
Bishwajit garai <102572447+Bishwajitgarai@users.noreply.github.com>
2025-12-23 19:47:40 +0530

    change in app

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from src.main import app
 def main():
     print("Hello from rag-fastapi!")
 
 
+
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
